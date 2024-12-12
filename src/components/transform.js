cf0cd7fbd2cbdd11ecb2ece9fdabb4061f979345
Sebastian <sebastian.kuhlgert@gmail.com>
2024-12-12 10:34:57 +0100

    Add additional transformation options

--- src/components/transform.js
+++ src/components/transform.js
@@ -19,10 +19,12 @@ export const settings = () => {
     "disabled": false,
     "options": [
       { "type": "select", "name": "fn", "value": "log₁₀", "options": [
-          {value: "log10", name: "log₁₀"}, 
-          {value: "ln2", name:  "ln₂"},
-          {value: "sqrt", name: "SQRT √"}, 
-          {value: "x2", name: "x²"}
+          {value: "log10", name: "log₁₀(x) - base 10 logarithm"}, 
+          {value: "log2", name:  "log₂(x) - base 2 logarithm"},
+          {value: "log", name:  "ln(x) - natural logarithm (base ℇ)"},
+          {value: "sqrt", name: "√ₓ - Square Root"}, 
+          {value: "x2", name: "x² - x squared"},
+          {value: "1/x", name: "¹∕ₓ - 1 over x"}
         ], "label": "Transform Data" },
     ]
   }
@@ -33,14 +35,20 @@ export const transformValue = (value, fn = null) => {
     case 'log10':
       value = Math.log10(value) || null;
       break;
-    case 'ln2':
+    case 'log2':
       value = Math.log2(value) || null;
+      break;
+    case 'log':
+      value = Math.log(value) || null;
       break;
     case 'sqrt':
       value = Math.sqrt(value) || null;
       break;
     case 'x2':
       value *= value || null; 
+      break;
+    case '1/x':
+      value = 1/value || null;
       break;
   }
   return value;
